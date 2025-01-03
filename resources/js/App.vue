010e1a1c25c4327016f376975ca04b30f9c2e7c9
Kirillmlk <kostakov.kiirill@yandex.by>
2025-01-03 18:55:36 +0300

    instaling sanctum

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,18 +1,49 @@
 <script>
 export default {
-    name: "App"
+    name: "Index",
+    data() {
+        return {
+            token: null
+        }
+    },
+
+    mounted() {
+        this.getToken()
+    },
+
+    watch: {
+        $route(to, from) {
+            this.getToken()
+        }
+    },
+
+    methods: {
+
+        getToken() {
+            this.token = localStorage.getItem('x_xsrf_token')
+        },
+
+        logout() {
+            axios.post('/logout')
+                .then( res => {
+                    localStorage.removeItem('x_xsrf_token')
+                    this.$router.push({name: 'user.login'})
+                })
+        }
+    }
 }
 </script>
 
 <template>
-    <div>
-        <h1 class="text-3xl font-bold ">
-            Hello world!
-        </h1>
-        <router-link to="index">Index component</router-link>
-        <router-link to="page">Page component</router-link>
-        <router-view></router-view>
-    </div>
+        <div>
+            <h1 class="text-3xl font-bold ">
+                Hello world!
+            </h1>
+            <router-link v-if="!token" :to="{ name: 'user.login'}">Login</router-link>
+            <router-link v-if="token" :to="{ name: 'user.personal'}">Personal</router-link>
+            <router-link v-if="!token" :to="{ name: 'user.registration'}">Registration</router-link>
+            <router-view></router-view>
+        </div>
 </template>
 
 <style scoped>
