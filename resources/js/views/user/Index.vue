a224dab08e02d02941e3c7d9dc3bf6f70bb23d7a
Kirillmlk <kostakov.kiirill@yandex.by>
2025-01-08 23:41:11 +0300

    Adding a user page

---
+++ resources/js/views/user/Index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="w-96 mx-auto">
+       <div>
+           Users
+       </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Index",
+
+    data() {
+
+    },
+
+    methods: {
+
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
